9636060d9ee22a6b9fb9756fe1d19d460e56d5f1
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-19 16:13:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,8 +3,3 @@ def square_array(array)
   array.each { |number| numbers <<  number ** 2 }
   numbers
 end
-
-#   new_numbers = []
-#   array.each { |i| new_numbers << i ** 2 }
-#   new_numbers
-# end
